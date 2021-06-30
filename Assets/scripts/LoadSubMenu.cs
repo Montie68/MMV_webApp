a8366af2970391a240b2efdb4909069384a1a133
geoff <geoff.newman@immersive.video>
2021-06-30 17:36:25 +1000

    eod 20210630

--- Assets/scripts/LoadSubMenu.cs
+++ Assets/scripts/LoadSubMenu.cs
@@ -11,11 +11,13 @@ public class LoadSubMenu : MonoBehaviour
     //private members go here
 
     [SerializeField] TextMeshProUGUI _levelTitleText;
-    private Museum _museumScriptable;
+    [SerializeField] TextMeshProUGUI _levelObjectText;
+    [SerializeField] TextMeshProUGUI _playButtonText;
     #endregion
 
     #region Public
     //public members go here
+    [HideInInspector] public Museum _museumScriptable;
 
     #endregion
     // Place all unity Message Methods here like OnCollision, Update, Start ect. 
@@ -24,7 +26,7 @@ public class LoadSubMenu : MonoBehaviour
     // Start is called before the first frame update
     // Update is called once per frame
 
-    void OnDisable() => _levelTitleText.text = "";
+    void OnDisable() => _levelObjectText.text = _levelTitleText.text = "";
     
     #endregion
     #region Public Methods
@@ -33,11 +35,18 @@ public class LoadSubMenu : MonoBehaviour
     {
         _museumScriptable = _museum;
         _levelTitleText.text = _museum.titleText;
+        string[] s = _museum.titleText.Split(' ');
+        _playButtonText.text = _playButtonText.text.Replace("[title]", s[0]);
+        int i = 1;
+        foreach (Museum.PlayObjects po in _museum.objectsToFind)
+        {
+            _levelObjectText.text += i++ + ".    <b>" + po.name + "</b><br> \n\r";
+            _levelObjectText.text += po.description + "<br><br> \n\r\n\r";
+        }
     }
     #endregion
     #region Private Methods
     //Place your public methods here
 
     #endregion
-
 }
